fd4af39e3572b165aeac2d601bcf5bf2b7bc6d0c
Roman Truschev <truschev@gmail.com>
2022-11-22 16:11:10 +0300

    simple plugin with url

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,4 +1,34 @@
-import Plugin from './plugin';
-import {id as pluginId} from './manifest';
+import React from "react";
+
+const pluginId = "com.mattermost.test-plugin";
+
+class Plugin {
+    initialize(registry, mmStore) {
+        const MainApp = () => {
+            return <h1>Main</h1>;
+        };
+
+        const HeaderComponent = () => {
+            return <h1>HeaderComponent</h1>;
+        };
+
+        const CentreHeaderComponent = () => {
+            return <h1>CentreHeaderComponent</h1>;
+        };
+
+        const Title = () => "Test plugin";
+
+        registry.registerProduct(
+            `/plugins/${pluginId}`,
+            "lock",
+            Title,
+            `/plugins/${pluginId}`,
+            MainApp,
+            CentreHeaderComponent, //() => null,
+            HeaderComponent,
+            false
+        );
+    }
+}
 
 window.registerPlugin(pluginId, new Plugin());
